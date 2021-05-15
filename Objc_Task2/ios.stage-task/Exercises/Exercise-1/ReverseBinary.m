37d437f87203b00a927a4f67596c096f2ce8cf89
Репина Надежда <repinanadezda@MacBook-Pro-Repina.local>
2021-05-15 23:42:45 +0300

    feat: add reverse binary task

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,19 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+    NSInteger tmpNumber = n;
+    NSString *binaryNumber = @"";
+    
+    while (tmpNumber > 0) {
+        binaryNumber = [binaryNumber stringByAppendingString: [NSString stringWithFormat:@"%lu", tmpNumber&1]];
+        tmpNumber = tmpNumber >> 1;
+    }
+    
+    for (NSInteger i = [binaryNumber length]; i < 8; i++) {
+        binaryNumber = [binaryNumber stringByAppendingString: @"0"];
+    }
+    
+    long decimalNumber = strtol([binaryNumber UTF8String], NULL, 2);
+    
+    return decimalNumber;
 }
