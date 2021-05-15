d7b70d97363a19c84a6b13b382ad4de35e7228bd
Репина Надежда <repinanadezda@MacBook-Pro-Repina.local>
2021-05-16 01:04:57 +0300

    fix: pairs task. change second cycle logic

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -5,7 +5,7 @@
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
     int count = 0;
         for (NSInteger i = 0; i < array.count; i++) {
-            for (NSInteger j = 0; j < array.count; j++) {
+            for (NSInteger j = i + 1; j < array.count; j++) {
                 if (array[i].integerValue + number.integerValue == array[j].integerValue) {
                     count++;
                 }
